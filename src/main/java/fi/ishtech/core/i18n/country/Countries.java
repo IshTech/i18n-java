4840998414bc4976de7daee18f700435d869012e
Muneer Syed <muneer@ishtech.fi>
2021-12-15 15:28:05 +0200

    country names by lang

---
+++ src/main/java/fi/ishtech/core/i18n/country/Countries.java
@@ -0,0 +1,67 @@
+package fi.ishtech.core.i18n.country;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import fi.ishtech.core.i18n.enums.CountryEnum;
+import fi.ishtech.core.i18n.enums.LangEnum;
+
+/**
+ *
+ * @author Muneer Ahmed Syed
+ */
+public abstract class Countries {
+
+	public static final Map<CountryEnum, String> byLang(LangEnum lang) {
+		if (lang == null) {
+			return null;
+		}
+
+		return MAP_LANG_COUNTRY_FULL_NAMES.get(lang);
+	}
+
+	public static final Map<CountryEnum, String> byLangOrElseByEn(LangEnum lang) {
+		Map<CountryEnum, String> tmpMapCountryFullName = MAP_LANG_COUNTRY_FULL_NAMES.get(lang);
+
+		return tmpMapCountryFullName != null ? tmpMapCountryFullName : byLang(LangEnum.en);
+	}
+
+	public static final Map<CountryEnum, String> byLang(String lang) {
+		return byLang(LangEnum.fromNameIgnoreCaseOrElseNull(lang));
+	}
+
+	public static final Map<CountryEnum, String> byLangOrElseByEn(String lang) {
+		return byLangOrElseByEn(LangEnum.fromNameIgnoreCaseOrElseNull(lang));
+	}
+
+	public static final Map<LangEnum, Map<CountryEnum, String>> byLangs(List<LangEnum> langs) {
+		if (langs == null) {
+			return null;
+		}
+
+		Map<LangEnum, Map<CountryEnum, String>> tmpLangMapCountryFullName = new HashMap<LangEnum, Map<CountryEnum, String>>(
+				langs.size());
+
+		for (LangEnum lang : langs) {
+			tmpLangMapCountryFullName.put(lang, MAP_LANG_COUNTRY_FULL_NAMES.get(lang));
+		}
+
+		return Collections.unmodifiableMap(tmpLangMapCountryFullName);
+	}
+
+	private static final Map<LangEnum, Map<CountryEnum, String>> MAP_LANG_COUNTRY_FULL_NAMES;
+	static {
+		Map<LangEnum, Map<CountryEnum, String>> tmpLangMapCountryFullName = new HashMap<LangEnum, Map<CountryEnum, String>>(
+				2);
+
+		tmpLangMapCountryFullName.put(LangEnum.en, CountriesEn.fullNames());
+		tmpLangMapCountryFullName.put(LangEnum.fi, CountriesFi.fullNames());
+
+		MAP_LANG_COUNTRY_FULL_NAMES = Collections.unmodifiableMap(tmpLangMapCountryFullName);
+
+		tmpLangMapCountryFullName = null; // gc
+	}
+
+}
---
+++ src/main/java/fi/ishtech/core/i18n/country/CountriesEn.java
@@ -0,0 +1,17 @@
+package fi.ishtech.core.i18n.country;
+
+import java.util.Map;
+
+import fi.ishtech.core.i18n.enums.CountryEnum;
+
+/**
+ *
+ * @author Muneer Ahmed Syed
+ */
+public abstract class CountriesEn {
+
+	public static final Map<CountryEnum, String> fullNames() {
+		return CountryEnum.fullNames();
+	}
+
+}
